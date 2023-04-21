46cdff66650471b0cdb3975930a71aed3783d9dd
MishaKozarev <mishaaaaa1986@mail.ru>
2023-04-21 23:06:25 +0300

    refactor(extended-1.2): add alt attribute

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="button delete";
   deleteButtonImg.src="./remove.svg";
   deleteButtonImg.className = 'btn-delete-img';
+  deleteButtonImg.alt = 'btn-delete';
   deleteButton.appendChild(deleteButtonImg);
 
 
